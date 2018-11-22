678aba4fe4a9219e5adde9edac594937a1d94623
Will <willyanglim@hotmail.com>
2018-11-22 15:46:36 +0000

    My first commit

--- server/server.js
+++ server/server.js
@@ -1,12 +1,34 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
-const publicPath = path.join(__dirname, '/../public')
+const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT || 3001;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(3001, () => {
-    console.log(`Server start, listening on ${port}!`);
+io.on('connection', (socket) => {
+console.log('New user connected');
+
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    });
+});
+
+server.listen(port, () => {
+    console.log(`Server started, listening on ${port}!`);
 });
